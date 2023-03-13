4228ce674c1883bc7baf0d61d8ada9c1d0afcf8c
ryeonnaderi <ryeonnaderi@gmail.com>
2023-03-12 20:38:34 -0400

    working on database and server

--- pages/Login.js
+++ pages/Login.js
@@ -1,14 +1,3 @@
-// import React from 'react'
-// import { View ,Stlesheet, Text } from 'react-native'
-// function Login() {
-//   return (
-//     <Text>Hello from Login</Text>
-//   )
-// }
-//
-// export default Login
-
-
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 
