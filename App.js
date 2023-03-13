028bd908a3c64ed516d10089cb74b9e8a541f213
Irfan Khan <KHANIRFANAHMAD1999@GMAIL.COM>
2023-03-13 00:22:46 -0400

    Updated the main app page to include the new register page

--- App.js
+++ App.js
@@ -6,11 +6,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './pages/Home';
 import List from './pages/List';
 import Login from './pages/Login';
+import Register from "./pages/Register";
 export default function App() {
   return (
     <NavigationContainer>
       <StackView.Navigator>
         <StackView.Screen name = "Login" component = {Login}/>
+        <StackView.Screen name = "Register" component = {Register}/>
         <StackView.Screen name = "Home" component = {Home}/>
         <StackView.Screen name = "List" component = {List}/>
 
