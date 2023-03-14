67731ff3037a950b1ee8746b7363e764357d6e60
ryeonnaderi <ryeonnaderi@gmail.com>
2023-03-13 20:13:49 -0400

    added python and pages

---
+++ pages/List.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+class List extends React.Component {
+    render() {
+     return (
+        <view style={style.container}>
+            <Text>Hello from List</Text>
+
+
+            <Button title='Home' onPress={() => this.prop.navigation.navigate("Home")}/>
+        </view>
+
+
+        )
+    }
+}
+
+export default List
