76fb4d3e05e5303125e6db8248ec695bdd2a714e
ryeonnaderi <ryeonnaderi@gmail.com>
2023-03-14 14:18:56 -0400

    connected firebase

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import {getFireStore, setDoc, doc} from "firebase/firestore"
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import list from "./pages/List";
 
 export default function App() {
+
+  // Initialize Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyCSfPj64r3ERrPyKv0ZkOu_W7dpl7deJiE",
+    authDomain: "cookbook-e93ca.firebaseapp.com",
+    databaseURL: "https://cookbook-e93ca-default-rtdb.firebaseio.com",
+    projectId: "cookbook-e93ca",
+    storageBucket: "cookbook-e93ca.appspot.com",
+    messagingSenderId: "575825664304",
+    appId: "1:575825664304:web:67759dece2c2f84e7110f5",
+    measurementId: "G-BBBHWLBKVQ"
+  };
+
+  initializeApp(firebaseConfig);
+  getAnalytics(app);
+
+  
+
+  const SendDatatoFirebase = async() =>{
+    const firestore = getFireStore();
+
+    await setDoc(doc(firestore, " user", "id"),{
+      phone: "48742424222",
+      name: "adada",
+      age: "19"
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Send data' onPress={SendDatatoFirebase} ></Button>
       <StatusBar style="auto" />
     </View>
   );
